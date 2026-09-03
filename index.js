396ae3698d9a91ab74bcdf119a8175ad44b31e8e
Udit Pandey <uditpandey61@gmail.com>
2026-09-03 12:15:27 +0530

    better commit mesg

--- index.js
+++ index.js
@@ -352,6 +352,24 @@
 // }
 // console.log(merge())
 
+// Find the common elements betwen the two arrays
+
+// let arr1 = [1,2,4,5]
+// let arr2 = [3,4,5]
+// let common = []
+// let k = 0
+// let fcommon = ()=>{
+//     for(let i = 0;i<arr1.length;i++){
+//         for(let j = 0;j<arr2.length;j++){
+//             if(arr1[i]===arr2[j]){
+//                 common[k] = arr1[i]
+//                 k++
+//             }
+//         }
+//     }
+//     return common
+// }
+// console.log(fcommon())
 
 
 
